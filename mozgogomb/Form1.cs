bbb3eb3fd98e26852e5f5ac552c9700d64a201cb
Gergely Csécsey <bcsopi01@gmail.com>
2017-09-29 22:14:21 +0200

    Add project files.

---
+++ mozgogomb/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        int[] tömb = new int[25];
+
+        public Form1()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 25; i++)
+            {
+                tömb[i] = i + 1;
+            }
+
+            Kever();
+            int sorszám = 0;
+            for (int sor = 0; sor < 5; sor++)
+                for (int oszlop = 0; oszlop < 5; oszlop++)
+                {
+                    Kepkocka k = new Kepkocka("lo_" + tömb[sorszám].ToString() + ".jpg", sor, oszlop);               
+                    this.Controls.Add(k);
+                    sorszám++;
+                }
+        }
+
+        void Kever()
+        {
+            Random rnd = new Random();
+            int n = 25;
+            for (int i = 0; i < n; i++)
+            {
+                int rdn = rnd.Next(n);
+                int köztes = tömb[i];
+                tömb[i] = tömb[rdn];
+                tömb[rdn] = köztes;
+            }
+        }
+
+
+
+    }
+}
---
+++ mozgogomb/Kepkocka.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    class Kepkocka : PictureBox
+    {
+
+        int ox, oy;
+        bool moving = false;
+
+        public Kepkocka(string kep, int sor, int oszlop)
+        {
+            MouseDown +=new MouseEventHandler(pbMouseDown);
+            MouseUp +=new MouseEventHandler(pbMouseUp);
+            MouseMove += new MouseEventHandler(pbMouseMove);
+
+            this.Load(kep);
+            this.Height = this.Image.Height;
+            this.Width = this.Image.Width;
+            this.Top = this.Image.Height * sor;
+            this.Left = this.Image.Width * oszlop;
+
+        }
+
+        private void pbMouseDown(object sender, MouseEventArgs e)
+        {
+            moving = true;
+            ox = e.X;
+            oy = e.Y;
+            BringToFront();
+        }
+
+        private void pbMouseUp(object sender, MouseEventArgs e)
+        {
+            moving = false;
+
+            
+
+        }
+
+        private void pbMouseMove(object sender, MouseEventArgs e)
+        {
+
+            if (moving)
+            {
+                ((PictureBox)sender).Left += e.X - ox;
+                ((PictureBox)sender).Top+= e.Y - oy;
+             
+            }
+
+        }
+
+        
+
+
+    }
+}
